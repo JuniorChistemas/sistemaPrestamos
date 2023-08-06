9c87b5eb6f8e9f32f2f30fae278b8abed80c1041
Junior15 <junior15062023@gmail.com>
2023-08-06 04:37:02 -0500

    login

---
+++ public/javascript/login.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        var loadingOverlay = document.querySelector('.dots-container');
+        loadingOverlay.classList.add('hide');
+    }, 2000);
+});
